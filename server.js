1895e5b6710b8cf14b6eecf453e8cdee84549554
Mynot <stefan.schweifer@gmail.com>
2021-12-20 22:31:28 +0100

    1 app code

--- server.js
+++ server.js
@@ -18,27 +18,26 @@ app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
 const port = 3000;
 const server = app.listen(port, listening);
 function listening() {
-    console.log(`running on localhost: ${port}`);
-};
+  console.log(`running on localhost: ${port}`);
+}
 
 //Get route
 app.get('/all', sendData);
 
-function sendDate(request, response) {
-    response.send(projectData);
-};
+function sendData(request, response) {
+  response.send(projectData);
+}
 
 //Post route
 app.post('/add', callBack);
 
 function callBack(request, response) {
-    response.send('Post received');
-};
+  response.send('Post received');
+}
 
 //Post add Data
 const data = [];
@@ -46,5 +45,5 @@ const data = [];
 app.post('/addData', addData);
 
 function addData(request, response) {
-    data.push(request.body);
-};
+  data.push(request.body);
+}
--- website/app.js
+++ website/app.js
@@ -2,4 +2,41 @@
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+//Post
+const postData = async (url = '', data = {}) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  try {
+    const newData = await response.json();
+    return newData;
+  } catch (error) {
+    console.log('error', error);
+  }
+};
+
+//Get
+const retrieveData = async (url = '') => {
+  const request = await fetch(url);
+  try {
+    const allData = await request.json();
+  } catch (error) {
+    console.log('error', error);
+  }
+};
+
+function postGet() {
+  postData('/addData', { date: newDate }).then(function (data) {
+    retrieveData('/all');
+  });
+}
+
+postGet();
